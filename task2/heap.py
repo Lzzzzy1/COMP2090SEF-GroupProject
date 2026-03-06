d4c5d0ede95e3895ed104d8cb0e2ee5b22c3056b
Lzzzzy1 <3353349327@qq.com>
2026-03-06 21:11:04 +0800

    Update heap.py

--- task2/heap.py
+++ task2/heap.py
@@ -1,1 +1,11 @@
+class Heap:
+    def __init__(self):
+        self.heap = []
 
+    def insert(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap)-1)
+
+    def _heapify_up(self, i):
+        p = (i-1)//2
+        if i > 0 and self.heap[i] > self.heap[p]:
